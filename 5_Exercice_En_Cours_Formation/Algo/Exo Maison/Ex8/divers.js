54fb35cd3c193f03ce135e9ee125e6d5a77a1ce5
Christopher Chiarandini <chiarandini.christopher@gmail.com>
2023-10-10 08:23:34 +0200

    ajout d'élément de fil rouge - enregistrement sécurité

--- 5_Exercice_En_Cours_Formation/Algo/Exo Maison/Ex8/divers.js
+++ 5_Exercice_En_Cours_Formation/Algo/Exo Maison/Ex8/divers.js
@@ -1,21 +1,10 @@
-// Exercice divers_1 :	écrire une fonction qui retourne **la position** du plus grand élément d'un tableau.
-// Exemple : positionBiggest([4,7,5,5,6]) retourne 1 car le plus grand est dans la case numéro 1
-
-function positionBiggest(tab) {
-  let biggest = tab[0];
-  let position = tab[0];
-  for (let i = 0; i < tab.length; i++) {
-    if (biggest < tab[i]) {
-      biggest = tab[i];
-      position = i;
-    }
+function goAlderande(x, y, z) {
+  while (10 * x > y) {
+    x = (y * z) % 10000;
+    y = (3 * z) % 10000;
+    z = (7 * z) % 10000;
   }
-  return position;
+  console.log(x, y, z);
 }
 
-console.log(positionBiggest([4, 7, 5, 5, 6, 72, -100, 254, 5, 9]));
-// Exercice divers_2 : écrire une fonction qui fait la somme de tous les éléments d'un tableau.
-
-// Exercice divers_3 : écrire une fonction qui retourne la somme des valeurs positives d'un tableau.
-
-// Exercice divers_4 : En utilisant les deux fonctions précédentes, écrire une fonction qui retourne la somme des valeurs négatives d'un tableau.
+goAlderande(997, 312, 663);
