6e76ae1c15f2b848925b70af5ea58513fc0ed14f
ChristopherTof <tofdevapp@gmail.com>
2023-10-16 17:56:01 +0200

    ajout fichier css et js

--- 5_Exercice_En_Cours_Formation/JS/5 - Exercice condition - 3/script.js
+++ 5_Exercice_En_Cours_Formation/JS/5 - Exercice condition - 3/script.js
@@ -4,13 +4,13 @@ Ecrire un algorithme qui demande 3 mots et affiche dans la console si ils sont t
 NB : les chaines de caractères peuvent être testé comme des nombres.
 */
 
-function toAlphabet(){
-   mot1 = prompt("Ecris le mot 1");
-   mot2 = prompt("Ecris le mot 2");
-   mot3 = prompt("Ecris le mot 3");
+function toAlphabet() {
+  mot1 = prompt("Ecris le mot 1");
+  mot2 = prompt("Ecris le mot 2");
+  mot3 = prompt("Ecris le mot 3");
 
-   if (mot1 > mot2 && mot1 > mot3);
-   console.log()
+  if (mot1 > mot2 && mot1 > mot3);
+  console.log();
 }
 
-toAlphabet()
+toAlphabet();
