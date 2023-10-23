5a6bcb059d8189c38b970b76d265cbf981e2c773
Christopher Chiarandini <chiarandini.christopher@gmail.com>
2023-10-23 19:07:02 +0200

    maj site fin d'étude

--- 5_Exercice_En_Cours_Formation/JS/DOM (Document Object Model)/EX5 Ajout Supprimer et refresh des elements/js/script.js
+++ 5_Exercice_En_Cours_Formation/JS/DOM (Document Object Model)/EX5 Ajout Supprimer et refresh des elements/js/script.js
@@ -1,40 +1,40 @@
 // Récupération des élément du DOM
-const body = document.querySelector("body");
-const tasks = document.querySelector("#tasks");
+const body = document.querySelector('body');
+const tasks = document.querySelector('#tasks');
 
 //Création de paragraphe au clic
 function addTask() {
   // Ajout d'un paragraphe et de son contenu
-  const taskValue = document.querySelector("#task").value;
+  const taskValue = document.querySelector('#task').value;
 
-  const para = document.createElement("p");
+  const para = document.createElement('p');
 
   //Partie Bonus
-  const divBonus = document.createElement("div");
-  divBonus.setAttribute("class", "container");
+  const divBonus = document.createElement('div');
+  divBonus.setAttribute('class', 'container');
   tasks.appendChild(divBonus);
   para.textContent = taskValue;
   divBonus.appendChild(para);
 
-  document.querySelector("#task").value = "";
+  document.querySelector('#task').value = '';
 
   //Creation bouton suppression
-  const btnDel = document.createElement("button");
+  const btnDel = document.createElement('button');
   divBonus.appendChild(btnDel);
-  btnDel.setAttribute("onClick", "deleteTask(this)");
-  btnDel.setAttribute("id", "delete");
-  btnDel.textContent = "Supprimer";
+  btnDel.setAttribute('onClick', 'deleteTask(this)');
+  btnDel.setAttribute('id', 'delete');
+  btnDel.textContent = 'Supprimer';
 
   //Creation bouton update
-  const btnUpdate = document.createElement("button");
+  const btnUpdate = document.createElement('button');
   divBonus.appendChild(btnUpdate);
-  btnUpdate.setAttribute("onClick", "updateTask(this)");
-  btnUpdate.setAttribute("id", "update");
-  btnUpdate.textContent = "Update";
+  btnUpdate.setAttribute('onClick', 'updateTask(this)');
+  btnUpdate.setAttribute('id', 'update');
+  btnUpdate.textContent = 'Update';
 }
 //Suppression des paragraphe
 function delAllTask() {
-  const supPara = document.querySelector("#tasks");
+  const supPara = document.querySelector('#tasks');
   while (supPara.firstChild) {
     // tant que tasks a un 1er enfant
     supPara.removeChild(supPara.firstChild); // tu supprime le premier
@@ -53,6 +53,6 @@ function deleteTask(btn) {
 }
 
 function updateTask(btn) {
-  target = btn.parentNode;
+  target = document.querySelector('container').nextSibling;
   console.log(target);
 }
