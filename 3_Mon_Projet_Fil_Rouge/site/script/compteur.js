95689da1b399fc493e1d9d703fb76a26d8b224e2
Christopher Chiarandini <chiarandini.christopher@gmail.com>
2023-11-05 14:47:16 +0100

    mise à jour du site web + modification du powerpoint pour l'oral

--- 3_Mon_Projet_Fil_Rouge/site/script/compteur.js
+++ 3_Mon_Projet_Fil_Rouge/site/script/compteur.js
@@ -1,41 +1,86 @@
+//Initialisation des varibales
+const body = document.querySelector('body');
 const map = document.querySelector('#worldMap');
+// Initialisation
+const tooltip = document.querySelector('#tooltip');
+tooltip.setAttribute('class', 'toooltip');
+tooltip.style.display = 'none';
+const sTPun = document.querySelector('#para .count');
+const sTPDeux = document.querySelector('#para2 .count');
 
-const france = map.querySelector('#FR');
-const usa = map.querySelector('#US');
+//Récupération de la valeur CSS d'un attribut (ici tooltip)
+const tooltipStyle = window.getComputedStyle(tooltip);
+const toolW = parseInt(tooltipStyle.width);
+const toolH = parseInt(tooltipStyle.height);
 
-france.addEventListener('click', () => {
-  console.log('Tu as cliqué sur la france !');
+//initialisation des chiffres d'émission a la seconde par pays (valeur tonnes)
+let fr = 10.2;
+
+// Compteur à la seconde pour un utilisateur français (appliqué dès le chargement de la page)
+//Lancement du compteur au chargement de la page
+window.addEventListener('load', () => {
+  cumulUser(0.15);
+  carbonCountry(fr);
 });
 
-usa.addEventListener('click', () => {
-  console.log('Tu as cliqué sur les USA !');
+//Fonction compteur information emission du User Français
+function cumulUser(qty) {
+  let gr = 0;
+  let dGr = 0;
+  let cGr = 0;
+  let kg = 0;
+
+  setInterval(function () {
+    sTPDeux.textContent = `${kg}.${cGr}${dGr}${parseInt(gr)} Kg`;
+    gr += qty;
+    console.log(gr);
+    if (gr >= 10) {
+      dGr += 1;
+      gr = gr - 10;
+    }
+    if (dGr > 9) {
+      cGr += 1;
+      dGr = 0;
+    }
+    if (cGr > 9) {
+      kg += 1;
+      cGr = 0;
+    }
+  }, 1000);
+}
+
+//Compteur par pays
+function carbonCountry(qty) {
+  let poid = 0;
+
+  setInterval(function () {
+    poid += qty / 10;
+    sTPun.textContent = parseInt(poid);
+  }, 100);
+}
+// Affichage du tooltip à l'endroit de la souris
+map.addEventListener('click', (evt) => {
+  let mouseX = evt.clientX + +window.scrollX; //propriété d'un mouseEvent qui donne la coordonée horizontale de la souris par rapport à la fenetre d'affichage (soit le 0,0 de la fenetre du nav)
+  let mouseY = evt.clientY + window.scrollY; //propriété d'un mouseEvent qui donne la coordonée verticale de la souris par rapport à la fenetre d'affichage (soit le 0,0 de la fenetre du nav)
+
+  // initialisation de la récupération du target
+  let cible = evt.target.id;
+  console.log(cible); // renvoi FR
+
+  if (tooltip.style.display == 'none') {
+    tooltip.style.display = 'block';
+    tooltip.style.transition = 'opacity 0.5s ease-in';
+    tooltip.style.opacity = '1';
+    tooltip.style.top = mouseY - (toolH + 10) + 'px';
+    tooltip.style.left = mouseX - 60 + 'px';
+  } else if (tooltip.style.display != 'none') {
+    tooltip.style.display = 'none';
+  }
+
+  map.addEventListener('mouseleave', () => {
+    console.log(cible);
+    setTimeout(() => {
+      tooltip.style.display = 'none';
+    }, 8000);
+  });
 });
-
-// const test = document.querySelector('#compteur');
-
-// let gr = 0;
-// let dGr = 0;
-// let cGr = 0;
-// let kg = 0;
-
-// function timer() {
-//   test.textContent = `Depuis que vous êtes sur cette pages, ${kg}.${cGr}${dGr}${gr}Kg Co2 ont été émis par un français`;
-//   setInterval(function () {
-//     test.textContent = `Depuis que vous êtes sur cette pages, ${kg}.${cGr}${dGr}${parseInt(gr)}Kg Co2 ont été émis par un français`;
-//     gr = gr + 1.5;
-
-//     if (gr >= 10) {
-//       gr = 0;
-//       dGr += 1;
-//     }
-//     if (dGr > 9) {
-//       dGr = 0;
-//       cGr += 1;
-//     }
-//     if (cGr > 9) {
-//       cGr = 0;
-//       kg += 1;
-//     }
-//   }, 10000);
-// }
-// timer();
